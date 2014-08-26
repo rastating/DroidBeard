14253392de6847975d71c87c3b44b2995b568ff0
rastating <rob@rastating.com>
2014-08-26 23:23:01 +0100

    Added loading animation prototype to show list
    Added prototype of animation for issue #10

--- app/src/main/java/com/rastating/droidbeard/fragments/ShowsFragment.java
+++ app/src/main/java/com/rastating/droidbeard/fragments/ShowsFragment.java
@@ -1,11 +1,18 @@
 package com.rastating.droidbeard.fragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.rastating.droidbeard.R;
@@ -18,6 +25,7 @@ public class ShowsFragment extends DroidbeardFragment implements AdapterView.OnI
     private ListView mListView;
     private View mErrorContainer;
     private TVShowSummaryAdapter mAdapter;
+    private ImageView mLoadingImage;
 
     public ShowsFragment() {
         setTitle(R.string.title_shows);
@@ -28,13 +36,23 @@ public class ShowsFragment extends DroidbeardFragment implements AdapterView.OnI
         View root = inflater.inflate(R.layout.fragment_show_list, container, false);
         mListView = (ListView) root.findViewById(R.id.show_list_view);
         mErrorContainer = root.findViewById(R.id.error_container);
+        mLoadingImage = (ImageView) root.findViewById(R.id.loading);
 
         if (mAdapter == null) {
+            mListView.setVisibility(View.GONE);
+            Animation a = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            a.setInterpolator(new LinearInterpolator());
+            a.setRepeatCount(-1);
+            a.setDuration(1000);
+            mLoadingImage.startAnimation(a);
+
             FetchShowSummariesTask task = new FetchShowSummariesTask(getActivity());
             task.addResponseListener(this);
             task.execute();
         }
         else {
+            mListView.setVisibility(View.VISIBLE);
+            mLoadingImage.setVisibility(View.GONE);
             mListView.setAdapter(mAdapter);
             mListView.setOnItemClickListener(this);
             mListView.setVisibility(View.VISIBLE);
@@ -61,7 +79,23 @@ public class ShowsFragment extends DroidbeardFragment implements AdapterView.OnI
             mListView.setAdapter(mAdapter);
 
             mListView.setOnItemClickListener(this);
+
+            mListView.setAlpha(0f);
             mListView.setVisibility(View.VISIBLE);
+            mLoadingImage.animate()
+                .alpha(0f)
+                .setDuration(500)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mLoadingImage.setVisibility(View.GONE);
+                        mListView.animate()
+                                .alpha(1f)
+                                .setDuration(500)
+                                .setListener(null);
+                    }
+                });
+
             mErrorContainer.setVisibility(View.GONE);
         }
         else {
